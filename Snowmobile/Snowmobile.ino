20e7f24fe8d89c4123940f45ac5bbc9fc7b7bc7d
James Grissom <72823098+JamesGrissom@users.noreply.github.com>
2022-05-30 17:49:52 -0400

    First upload for the snowmobile project

---
+++ Snowmobile/Snowmobile.ino
@@ -0,0 +1,12 @@
+const int key = 2;  //turning the key connects the yellow and blue wires
+const int throttle = A0;  //Signal from the green wire
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
