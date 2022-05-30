e71a1eaeaa4d9829624b49684b590b270b271c60
James Grissom <72823098+JamesGrissom@users.noreply.github.com>
2022-05-30 19:20:25 -0400

    Update Snowmobile.ino

--- Snowmobile/Snowmobile.ino
+++ Snowmobile/Snowmobile.ino
@@ -3,7 +3,10 @@ const int engineStart = 3; // Triggers the engine starting sound
 const int engineRun  = 4; // Tiggers the engine running sound
 const int headlight = 5;  // Turns on the headlight
 const int taillight = 6;  // Turns on the taillight
+const int volumeUp = 7;   // Turns up the engine volume
+const int volumeDown = 8; // Turns down the engine volume
 const int throttle = A0;  // Signal from the green wire - range from around 340 to about 415 at 5v
+int volumeCount = 0
 
 void setup() {
   pinMode(key, INPUT);
@@ -24,10 +27,17 @@ void loop() {
     engineRun = LOW;
     headlight = HIGH;
     tailLight = HIGH;
+    while((analogRead(throttle) >= 375)) & (volumeCount < 5)) {
+      volumeUp = LOW;
+      volumeUp = HIGH;
+      volt
+    }
   }
   else if (keyStart == LOW) {
     engineStart = HIGH;
     engineRun = HIGH;
+    volumeUp = HIGH;
+    volumeDown = HIGH;
     headlight = LOW;
     taillight = LOW;
   }
