6aba2d3b31ee6a05ad7075b3efc3ada95d2547e2
James Grissom <72823098+JamesGrissom@users.noreply.github.com>
2022-05-30 18:50:21 -0400

    Second Snowmobile upload
    wee need another adafruit audio fx sound board - the one with the audio jack and volume control

--- Snowmobile/Snowmobile.ino
+++ Snowmobile/Snowmobile.ino
@@ -1,12 +1,34 @@
-const int key = 2;  //turning the key connects the yellow and blue wires
-const int throttle = A0;  //Signal from the green wire
+const int keyStart = 2;  // Turning the key connects the yellow and blue wires - starts the snowmobile
+const int engineStart = 3; // Triggers the engine starting sound
+const int engineRun  = 4; // Tiggers the engine running sound
+const int headlight = 5;  // Turns on the headlight
+const int taillight = 6;  // Turns on the taillight
+const int throttle = A0;  // Signal from the green wire - range from around 340 to about 415 at 5v
 
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(key, INPUT);
+  pinMode(throttle, INPUT);
+  pinMode(engineStart, OUTPUT);
+  pinMode(engineRun, OUTPUT);
+  pinMode(headlight, OUTPUT);
+  pinMode(taillight, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println(analogRead(throttle));
+  if (keyStart == HIGH) {
+    engineStart = LOW;
+    delay(1000);
+    engineStart = HIGH;
+    engineRun = LOW;
+    headlight = HIGH;
+    tailLight = HIGH;
+  }
+  else if (keyStart == LOW) {
+    engineStart = HIGH;
+    engineRun = HIGH;
+    headlight = LOW;
+    taillight = LOW;
+  }
 }
